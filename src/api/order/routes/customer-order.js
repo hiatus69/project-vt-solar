43134055f813f11482dbd37e57630156f93dcc50
hiatus69 <narathip.fluk@gmail.com>
2025-07-28 22:53:41 +0700

    Update customer-order.js

--- src/api/order/routes/customer-order.js
+++ src/api/order/routes/customer-order.js
@@ -2,12 +2,8 @@ module.exports = {
   routes: [
     {
       method: 'GET',
-      path: '/orders/me', // นี่คือ URL ของถนนเส้นใหม่ของเรา
-      handler: 'custom-order.me', // บอกว่าให้ไปเรียกใช้ฟังก์ชัน me จากไฟล์ custom-order
-      config: {
-        policies: [],
-        middlewares: [],
-      },
+      path: '/orders/me',
+      handler: 'custom-order.me',
     },
   ],
 };
